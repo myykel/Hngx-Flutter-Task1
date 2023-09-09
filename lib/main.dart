ca912cb0efd463dd2a9bac8136bc22f4f808a71e
Michael Eboji <mac@macs-MacBook-Pro.local>
2023-09-09 21:34:17 +0100

    added some files

--- lib/main.dart
+++ lib/main.dart
@@ -19,39 +19,39 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Slack & GitHub App'),
+        title: const Text('My Slack & GitHub App'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Slack Name: myykel',
               style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Image.network(
               'https://ca.slack-edge.com/T05FFAA91JP-U05R8L0MVMZ-1742375011ef-512',
               width: 120,
               height: 120,
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(
                   builder: (BuildContext context) {
                     return Scaffold(
                       appBar: AppBar(
-                        title: Text('GitHub Profile'),
+                        title: const Text('GitHub Profile'),
                       ),
-                      body: WebView(
+                      body: const WebView(
                         initialUrl: 'https://github.com/myykel',
                       ),
                     );
                   },
                 ));
               },
-              child: Text('Open GitHub'),
+              child: const Text('Open GitHub'),
             ),
           ],
         ),
